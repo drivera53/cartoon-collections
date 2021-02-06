3da491a42d6a93fbe529a27e9fdbcae0309e8d72
Danny Rivera <samboard@hotmail.com>
2021-02-06 10:35:06 -0500

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each.with_index(1) do |dwarve, index| # We can also use "collect.with_index"
+    puts "#{index}. #{dwarve}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planateer_calls)# code an argument here
+  capitalized = planateer_calls.collect do |word|
+    word.capitalize << "!"
+  end
+  capitalized
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array_of_calls)# code an argument here
+  return_value = false
+  array_of_calls.each do |calls|
+    if calls.size > 4
+      return_value = true
+    end
+  end
+  return return_value
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array_of_food)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array_of_food.each do |food|
+    return_value = cheese_types.include? food
+    if return_value == true
+      return food
+    end
+  end
+  yield if block_given?
 end
+
+#binding.pry
